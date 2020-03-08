ddbaf431766b66c3cc3e6d022f9a16ac95dc1743
蒋雪文 <445251692@qq.com>
2020-03-08 17:15:59 +0800

    Initial commit

---
+++ src/main/java/com/judream/teamcity/TeamCityApplication.java
@@ -0,0 +1,13 @@
+package com.judream.teamcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamCityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/judream/teamcity/TeamCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.judream.teamcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamCityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
